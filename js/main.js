1d98bb5748c8afacb21563fc76f8f8e505e8ec1a
anasibrahimgm <anasibrahimgm@gmail.com>
2017-05-14 21:18:20 +0200

    navbar

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(function(){
+	'use strict';
+	$(window).scroll(function(){
+		/*window.console.log('navbar height ' + $('.navbar').height());
+		window.console.log('window scrollTop: ' + $(window).scrollTop());*/
+		var navbar = $('.navbar');
+		if($(window).scrollTop() >= $('.navbar').height()){
+			if(!navbar.hasClass('scrolled')){
+				navbar.addClass('scrolled');				
+			}
+		} else {
+			navbar.removeClass('scrolled');
+		}
+	});
+});

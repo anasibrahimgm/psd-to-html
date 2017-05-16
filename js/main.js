82da45e2025407bc4e4b58aab5dc2fe33d3a51a7
anasibrahimgm <anasibrahimgm@gmail.com>
2017-05-16 17:44:39 +0200

    Tabs

--- js/main.js
+++ js/main.js
@@ -12,4 +12,17 @@ $(function(){
 			navbar.removeClass('scrolled');
 		}
 	});
+
+	//tabs-section
+	$('.tab-switch li').click(function() {
+		//add 'selected' class to active div and remove it from other 'li's
+		$(this).addClass('selected').siblings().removeClass('selected');
+
+		//hide all divs
+		$('.tabs-section .tabs-content > div').hide();
+		//window.console.log($(this).data('class'));
+
+		//show div connected to this link
+		$('.' + $(this).data('class')).show();
+	});
 });
